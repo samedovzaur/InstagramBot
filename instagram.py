f3f5af8edd1de4f6db3a6c252f627a8ee832912f
zaursamedov2000 <101812544+zaursamedov2000@users.noreply.github.com>
2022-07-14 22:39:50 +0300

    followers list with scrolling

--- instagram.py
+++ instagram.py
@@ -1,5 +1,6 @@
 # Firstly to try it out you got to type your username and password into instagramUserInfo.py  
 
+from numpy import true_divide
 from instagramUserInfo import username,password
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -18,15 +19,33 @@ class Instagram:
         passwordInput.send_keys(self.password)
         passwordInput.send_keys(Keys.ENTER)
         time.sleep(2)
-    def getFollowers(self):
+    def getFollowers(self,dialog):
+             self.dialog=dialog
              self.browser.get(f"https://www.instagram.com/{self.username}")
              time.sleep(2)
 
              self.browser.find_element('xpath','//*[@id="mount_0_0_S4"]/div/div[1]/div/div[1]/div/div/div/div[1]/div[1]/section/main/div/header/section/ul/li[2]/a/div/span').click()
              time.sleep(2)
              followers=self.browser.find_element('cssSelector','div[role=dialog] ul').find_element('cssSelector','li')
+             dialog=self.browser.find_element('cssSelector','div[role=dialog] ul')
+             followercount=len(dialog.find_element('cssSelector','li'))
+             print(f('first count {followercount}'))
+             action=webdriver.ActionChains(self.browser)
+             user=self.browser.find_element('cssSelector','a')
+             while True:
+                dialog.click()
+                action.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
+                time.sleep(2)
+                newcount=len(dialog.find_element('cssSelector','li'))
+                if followercount!=newcount:
+                    followercount=newcount
+                    print(f'second count:{newcount}')
+                    pass
+                else:
+                    break
+             
              for user in followers:
-                link=user.find_element('cssSelector','a').get_attribute('href')
+                link=self.browser.find_element('cssSelector','a').get_attribute('href')
                 print(link)
 
     
